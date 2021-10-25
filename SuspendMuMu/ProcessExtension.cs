363a9fc5be564e06df088a92eeb85cb399e05202
Akitsuki <18420831+qiuyue0@users.noreply.github.com>
2021-10-25 10:39:38 +0800

    使用丢弃

--- SuspendMuMu/ProcessExtension.cs
+++ SuspendMuMu/ProcessExtension.cs
@@ -25,7 +25,7 @@ public static class ProcessExtension
             {
                 break;
             }
-            SuspendThread(pOpenThread);
+            _ = SuspendThread(pOpenThread);
         }
     }
 
