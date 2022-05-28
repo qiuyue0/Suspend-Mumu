a842842a26be37ae8dac77dc86aa0556e04becce
Akitsuki <18420831+qiuyue0@users.noreply.github.com>
2022-05-28 19:06:43 +0800

    修改重启按钮为更新pid

--- SuspendMuMu/Form1.Designer.cs
+++ SuspendMuMu/Form1.Designer.cs
@@ -183,7 +183,7 @@ namespace BOWKeyBoardHook
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(80, 30);
             this.button3.TabIndex = 12;
-            this.button3.Text = "ÖØÆô";
+            this.button3.Text = "¸üÐÂPID";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
