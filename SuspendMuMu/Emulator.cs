1efc0b24965b52df4e4b8617041b08c459bbd708
Akitsuki <18420831+qiuyue0@users.noreply.github.com>
2022-05-28 14:11:50 +0800

    修复了卡键盘的问题

--- SuspendMuMu/Emulator.cs
+++ SuspendMuMu/Emulator.cs
@@ -10,34 +10,45 @@ namespace SuspendMuMu
     {
         public static int GetEmulator(string ProcessName,string whichone)
         {
-            //List<int> result = new List<int>();
-            string wmiQuery = string.Format("select CommandLine,Handle from Win32_Process where Name='{0}'", ProcessName);
+            List<int> result = new List<int>();
+            string wmiQuery = string.Format("select ThreadCount,Handle from Win32_Process where Name='{0}'", ProcessName);
             using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(wmiQuery))
             {
                 using (ManagementObjectCollection retObjectCollection = searcher.Get())
                 {
                     foreach (ManagementObject retObject in retObjectCollection)
                     {
-                        string[] commandline = Regex.Split((string)retObject["CommandLine"], "\\s", RegexOptions.IgnoreCase);
+                        uint ThreadCount = (uint)retObject["ThreadCount"];
+                        int Handle = int.Parse((string)retObject["Handle"]);
+                        if (ThreadCount > 100)
+                        {
+                            result.Add(Handle);
+                        }
+
+                    }
+                    try
+                    {
                         if (whichone == "主窗口")
                         {
-                            if (commandline[5] == "0")
-                            {
-                                return int.Parse(retObject["Handle"].ToString());
-                            }
+                            return result[0];
                         }
-                        else if (whichone == "多开窗口")
+                        else if (whichone == "多开窗口1")
                         {
-                            if (commandline[5] == "1")
-                            {
-                                return int.Parse(retObject["Handle"].ToString());
-                            }
+                            return result[1];
+                        }
+                        else if (whichone == "多开窗口2")
+                        {
+                            return result[2];
                         }
                     }
+                    catch
+                    {
+                        return -1;
+                    }
                 }
+                return -1;
             }
-            return -1;
+
         }
-        
     }
 }
