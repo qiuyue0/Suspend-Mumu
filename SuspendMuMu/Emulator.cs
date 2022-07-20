b97a579734e7a47d67cf2333aa90b88a10c684fb
Akitsuki <18420831+qiuyue0@users.noreply.github.com>
2022-07-21 03:38:10 +0800

    fix: 弃用不可靠的wmi判断方法

--- SuspendMuMu/Emulator.cs
+++ SuspendMuMu/Emulator.cs
@@ -2,50 +2,55 @@
 using System.Management;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace SuspendMuMu
 {
-    // 现在是不可用状态
     public static class Emulator
     {
-        public static int GetEmulator(string ProcessName,string whichone)
+        public static int GetEmulator(string ProcessName, string whichone)
         {
-            List<int> result = new List<int>();
-            string wmiQuery = string.Format("select ThreadCount,Handle from Win32_Process where Name='{0}'", ProcessName);
-            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(wmiQuery))
+            try
             {
-                using (ManagementObjectCollection retObjectCollection = searcher.Get())
+                List<Process> result = new List<Process>();
+                // 麻了，终于找到了可靠的判断方法
+                Process[] processes = Process.GetProcessesByName(ProcessName);
+                foreach (Process process in processes)
                 {
-                    foreach (ManagementObject retObject in retObjectCollection)
+                    if (GetCommandLine.IsPcr(process))
                     {
-                        uint ThreadCount = (uint)retObject["ThreadCount"];
-                        int Handle = int.Parse((string)retObject["Handle"]);
-                        if (ThreadCount > 110) result.Add(Handle);
-
-                    }
-                    try
-                    {
-                        if (whichone == "主窗口")
-                        {
-                            return result[0];
-                        }
-                        else if (whichone == "多开窗口1")
-                        {
-                            return result[1];
-                        }
-                        else if (whichone == "多开窗口2")
-                        {
-                            return result[2];
-                        }
-                    }
-                    catch
-                    {
-                        return -1;
+                        result.Add(process);
                     }
                 }
+                result.Sort(SortTime);
+                int i;
+                if (whichone == "主窗口" && result.Count >= 1) i = 0;
+                else if (whichone == "多开窗口1" && result.Count >= 2) i = 1;
+                else if (whichone == "多开窗口2" && result.Count >= 3) i = 2;
+                // fallback 默认取最后开的窗口
+                else i = result.Count;
+
+
+                return result[i].Id;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+        private static int SortTime(Process A, Process B)
+        {
+            DateTime creatTimeA = A.StartTime;
+            DateTime creatTimeB = B.StartTime;
+            if (creatTimeA > creatTimeB)
+            {
+                return 1;
+            }
+            else if (creatTimeA < creatTimeB)
+            {
                 return -1;
             }
-
+            return 0;
         }
     }
 }
